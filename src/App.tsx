32f619709745f2e3112eab334315d55a9a0cd4ab
Renan Moreira <renan.tsx@gmail.com>
2022-10-31 21:15:18 -0300

    adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+
+import { Box } from './components/Box';
+import { Container } from './components/Container';
 import { Header } from './components/Header';
+import { Newtask } from './components/NewTask';
+import { StatusTasks } from './components/StatusTasks';
+import { Task } from './components/Task';
 import './global.scss';
 
 function App() { 
   return (
-    <Header />
+    <>
+      <Header />
+      <Container>
+        <Newtask />
+        <StatusTasks />
+        <Box mt={8}>
+          <Task />
+          <Task />
+          <Task />
+        </Box>
+      </Container>
+    </>
   )
 }
 
